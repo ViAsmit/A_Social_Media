4ec9fd7f647072b0190b3e87373845c11ccc108c
ViAsmit <asmit.vml.962@gmail.com>
2021-01-11 22:26:50 +0530

    CORS fix

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(function(req, res, next) {
 app.use(passport.initialize());
 
 app.use('/', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', 'http://whispering-hollows-06439.herokuapp.com/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
